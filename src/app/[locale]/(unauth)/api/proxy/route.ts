92637e249dc1034e537d50191faf90498dac70d1
bozodesign <bozoce@yahoo.co.th>
2024-09-16 21:37:10 +0700

    get json data from proxy

--- src/app/[locale]/(unauth)/api/proxy/route.ts
+++ src/app/[locale]/(unauth)/api/proxy/route.ts
@@ -10,46 +10,10 @@ export async function GET() {
 
     // Load HTML into Cheerio
     const $ = cheerio.load(html);
-
-    // Extract data from the HTML
-    const posts: any[] = [];
-    $('ul.pt-list > li.pt-list-item').each((i, element) => {
-      // Extract title and link
-      const title = $(element).find('.pt-list-item__title h2 a').text().trim();
-      const link = $(element).find('.pt-list-item__title h2 a').attr('href');
-
-      // Extract thumbnail image URL
-      const thumbnailElement = $(element).find('.pt-list-item__img');
-      let thumbnailUrl = thumbnailElement.attr('data-bg');
-
-      if (!thumbnailUrl) {
-        // Fallback to extracting from the style attribute
-        const style = thumbnailElement.attr('style');
-        const match = style?.match(/url\(["']?([^"']*)["']?\)/);
-        if (match) {
-          thumbnailUrl = match[1];
-        }
-      }
-
-      const tags: any[] = [];
-      $(element).find('.pt-list-item__tag a').each((j, tagElement) => {
-        tags.push($(tagElement).text().trim());
-      });
-
-      // Extract the author and profile link
-      const authorElement = $(element).find('.pt-list-item__info h5 a');
-      const author = authorElement.text().trim();
-      const authorProfileLink = authorElement.attr('href');
-
-      // Extract comment count and votes
-      const commentCount = $(element).find('.pt-li_stats-comment').text().trim();
-      const votes = $(element).find('.pt-li_stats-vote').text().trim();
-
-      posts.push({ title, link, thumbnailUrl, tags, author, authorProfileLink, commentCount, votes });
-    });
-
+    const nextDataScript: any = $('#__NEXT_DATA__').html();
+    const nextData = JSON.parse(nextDataScript);
     // Return the extracted data as JSON
-    return NextResponse.json({ posts }, { status: 200 });
+    return NextResponse.json({ nextData }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ error: `Error fetching data: ${error.message}` }, { status: 500 });
   }
