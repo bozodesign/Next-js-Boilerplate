2d168e9553dc6ceeb3454a4d8a0ea7f30cfa667a
bozodesign <bozoce@yahoo.co.th>
2024-09-17 23:14:14 +0700

    update Rooms skeleton

--- src/app/[locale]/(unauth)/api/topic/[tid]/route.ts
+++ src/app/[locale]/(unauth)/api/topic/[tid]/route.ts
@@ -4,15 +4,16 @@ import fetch from 'node-fetch';
 
 export async function GET(req: Request, { params }: { params: { tid: string } }) {
   const topicId = params.tid;
+  req.headers.set('content-type', 'application/json');
   try {
     // Fetch the HTML from Pantip
-    const response = await fetch('https://pantip.com/topic/' + topicId);
+    const response = await fetch(`https://pantip.com/topic/${topicId}`);
     const html = await response.text();
 
     // Load HTML into Cheerio
     const $ = cheerio.load(html);
-    const title:string = $('h2.display-post-title').text();
-    const story:string = $('div.display-post-story').text();
+    const title: string = $('h2.display-post-title').text();
+    const story: string = $('div.display-post-story').text();
     const author: string = $('a.display-post-name.owner').text();
     const imageUrl: string = $('a.signature-user img').attr('src') || '';
     const profileId: string = $('a.display-post-name.owner').attr('href')?.split('/profile/')[1] || '';
@@ -20,18 +21,18 @@ export async function GET(req: Request, { params }: { params: { tid: string } })
     const likeCount: string = $('span.like-score').text();
     const emotionCount: string = $('span.emotion-score').text();
 
-    
-    const topicData = { 
+    const topicData = {
       title,
       story,
       postDate,
-      author:{
-          name:author,
-          imageUrl,
-          profileId},
+      author: {
+        name: author,
+        imageUrl,
+        profileId,
+      },
       likeCount,
-      emotionCount  
-        }
+      emotionCount,
+    };
 
     return NextResponse.json(topicData, { status: 200 });
   } catch (error: any) {
